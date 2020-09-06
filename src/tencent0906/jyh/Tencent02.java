8b7f34ef9bbcbd776cc772efb6d4eb55a8fcfe94
Jiyonghao <767883085@qq.com>
2020-09-06 20:54:19 +0800

    ten02

---
+++ src/tencent0906/jyh/Tencent02.java
@@ -0,0 +1,79 @@
+package tencent0906.jyh;
+
+import java.lang.reflect.Array;
+import java.util.*;
+
+public class Tencent02 {
+
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int m = in.nextInt();
+        List<HashSet<Integer>> list = new ArrayList<>();
+//        HashMap<Integer,ArrayList<Integer>> map = new HashMap<>();
+        HashSet<Integer> set = new HashSet<Integer>();
+        for (int i = 0; i < m; i++) {
+            int num = in.nextInt();
+//            List<Integer> tmpList = new ArrayList<>();
+            HashSet<Integer> tmpSet = new HashSet<>();
+            int flag = 0;
+            for (int j = 0; j < num; j++) {
+                int tmp = in.nextInt();
+                tmpSet.add(tmp);
+                if(tmp == 0){
+                    flag = 1;
+                }
+            }
+            list.add(tmpSet);
+            if(flag == 1)set.addAll(tmpSet);
+//            if(flag == 1){
+//                for (int j = 0; j < tmpList.size(); j++) {
+//                    set.add(tmpList.get(j));
+//                }
+//                set.addAll(tmpList);
+            }
+
+        for (int i = 0; i < list.size(); i++) {
+            Iterator<Integer> it = list.get(i).iterator();
+            while(it.hasNext()) {
+                if(set.contains(it.next())) {
+                    set.addAll(list.get(i));
+                    break;
+                }
+            }
+        }
+        System.out.println(set.size());
+    }
+
+//    public static void main(String[] args) {
+//        Scanner in = new Scanner(System.in);
+//        int n = in.nextInt();
+//        int m = in.nextInt();
+//        List<ArrayList<Integer>> list = new ArrayList<>();
+//        HashMap<Integer,ArrayList<Integer>> map = new HashMap<>();
+//        for (int i = 0; i < m; i++) {
+//            list.add(new ArrayList<Integer>());
+//        }
+//        for (int i = 0; i < m; i++) {
+//            int num = in.nextInt();
+//            List<Integer> tmpList = new ArrayList<>();
+//            for (int j = 0; j < num; j++) {
+//                int tmp = in.nextInt();
+//                tmpList.add(tmp);
+////                if(!map.containsKey(tmp)){
+////                    ArrayList<Integer> tList = new ArrayList<>();
+////                    tList.add(i);
+////                    map.put(tmp,tList);
+////                }
+////                else {
+////                    ArrayList<Integer> tt = map.get(tmp);
+////                    tt.add(i);
+////                    map.put(tmp,tt);
+////                }
+//            }
+//            list.get(i).addAll(tmpList);
+//        }
+//
+//    }
+}
