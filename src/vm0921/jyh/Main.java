16202196b16975786d81046bf4bbafbe9164ad23
wangyao2221 <wangyao2221@163.com>
2020-09-23 19:58:53 +0800

    去哪儿 02

--- src/vm0921/jyh/Main.java
+++ src/vm0921/jyh/Main.java
@@ -1,8 +1,5 @@
 package vm0921.jyh;
 
-import javafx.beans.binding.DoubleExpression;
-
-import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Main {
@@ -10,54 +7,65 @@ public class Main {
         Scanner in = new Scanner(System.in);
         double n = in.nextDouble();
         int m = in.nextInt();
-        double workLen = in.nextDouble();
+        double step = in.nextDouble();
+
+        resolve(n, m, step);
+    }
+
+    private static void resolve(double n, int m, double step) {
         double x = 0;
         double y = 0;
-        DecimalFormat df = new DecimalFormat("0.00");
         double tmp = 0;
-        while (m-- > 0){
-            double r = workLen;
-//            int times = (int) ((int)r / (4 * n));
-//            r = r - times*n*4;
+
+        double a = 0;
+        double b = 0;
+        while (m-- > 0) {
+            double r = step;
             while (r > 0) {
                 if (x < n && y == 0) {
                     tmp = x;
                     x += r;
+                    b = x;
                     if (x > n) {
                         x = n;
                         r -= n - tmp;
+                    } else {
+                        r = 0;
                     }
-                    else r = 0;
                 }
                 if (x == n && y < n) {
                     tmp = y;
                     y += r;
+                    b = y;
                     if (y > n) {
                         y = n;
                         r -= n - tmp;
+                    } else {
+                        r = 0;
                     }
-                    else r = 0;
                 }
                 if (x > 0 && y == n) {
                     tmp = x;
                     x -= r;
+                    b = x;
                     if (x < 0) {
                         x = 0;
-                        r -= tmp - 0;
-                    }
-                    else r = 0;
+                        r -= tmp;
+                    } else r = 0;
                 }
                 if (x == 0 && y > 0) {
                     tmp = y;
                     y -= r;
+                    b = y;
                     if (y < 0) {
                         y = 0;
-                        r -= tmp - 0;
+                        r -= tmp;
+                    } else {
+                        r = 0;
                     }
-                    else r = 0;
                 }
             }
-            System.out.println(df.format(x)+" "+df.format(y));
+            System.out.printf("%.2f %.2f\n", x, y);
         }
     }
 }
